8f0ae30d7daebdb379fb54dac2e9b368d82e6d22
diatoub <khadidiatoubeye@hotmail.com>
2023-08-25 13:56:39 +0000

    new commit

---
+++ seminaire_pdo/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Séminaire</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+<?php include "menu.php"; ?>
+<h1>Bienvenue sur le site de formation!</h1>
+
+</body>
+</html>
---
+++ seminaire_pdo/menu.php
@@ -0,0 +1,8 @@
+<div id="menu">
+<ul>
+    <li><a href="index.php">Accueil</a></li>
+    <li><a href="form_inscription.php">Inscription</a></li>
+    <li><a href="liste_participants.php">liste des participants</a></li>
+    <li><a href="admin/index.php">Administration</a></li>
+</ul>
+</div>
